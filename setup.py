76f0421952c416df466b6dea08a20c768848f9ca
JckHamm3r <80009048+JckHamm3r@users.noreply.github.com>
2025-04-06 22:09:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="sonar_software_api",
     version="0.1.0",
     description="A simple GraphQL client for interacting with your Sonar Software instance",
-    author="Jckhmr",
+    author="JckHamm3r",
     packages=find_packages(),
     install_requires=[
         "gql>=3.4.0",
