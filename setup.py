2c8bb995a8a21aaff81ad1bf07b364beb209e3df
JckHamm3r <80009048+JckHamm3r@users.noreply.github.com>
2025-04-16 17:35:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="sonar_software_api",
-    version="0.1.4",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     description="A simple GraphQL client for interacting with your Sonar Software instance",
     author="JckHamm3r",
     packages=find_packages(),
